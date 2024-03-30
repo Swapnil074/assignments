cd5bd5556095bd82c4cc702663452b97c14e5e84
Swapnil074 <mailswapnil0074@gmail.com>
2024-03-30 09:22:44 +0530

    step-13: account transfer

--- week-8.2/paytm/backend/db/index.js
+++ week-8.2/paytm/backend/db/index.js
@@ -8,7 +8,7 @@ const userSchema=new mongoose.Schema({
     lastName:String,
 })
 const accountSchema=new mongoose.Schema({
-    user:{
+    userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
         required:'true'
--- week-8.2/paytm/backend/routes/account.js
+++ week-8.2/paytm/backend/routes/account.js
@@ -1,6 +1,53 @@
 const {Router}=require('express')
 const zod=require('zod')
 const {authMiddleware}=require('../middleware/index')
+const { Account } = require('../db')
+const { default: mongoose } = require('mongoose')
 const router=Router()
+
+const balanceBody=zod.object({
+    balance:zod.number()
+})
+
+router.get('/balance',authMiddleware,async(req,res)=>{
+    const userId=req.userId
+    const account=await Account.findOne({
+        user:userId
+    })
+    res.status(200).json({
+        balance: account?.balance
+    })
+})
+router.post('/transfer',authMiddleware,async(req,res)=>{
+    const userId=req.userId
+    const session=await mongoose.startSession()
+    session.startTransaction()
+    const {amount, to}=req.body
+    const account=await Account.findOne({
+        userId:userId
+    }).session(session)
+
+    if(!account || account.balance<amount){
+        await session.abortTransaction()
+        return res.status(400).json({
+            	message: "Insufficient balance"
+        })
+    }
+    const toAccount=await Account.findOne({
+        userId:to
+    }).session(session)
+    if(!toAccount){
+        await session.abortTransaction()
+        return res.status(400).json({
+            message:"Invalid account"
+        })
+    }
+    await Account.updateOne({userId:userId},{$inc:{balance:-amount}}).session(session)
+    await Account.updateOne({userId:to},{$inc:{balance:amount}}).session(session)
+    await session.commitTransaction()
+    res.json({
+        message: "Transfer succesful"
+    })
+})
 
 module.exports=router
