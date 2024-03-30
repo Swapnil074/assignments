256af7ad3b2f12087601cf47afb261f560ebcfca
Swapnil074 <mailswapnil0074@gmail.com>
2024-03-30 08:09:40 +0530

    Step-8: update userinfo

--- week-8.2/paytm/backend/routes/user.js
+++ week-8.2/paytm/backend/routes/user.js
@@ -3,7 +3,7 @@ const zod = require("zod");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
 const { User } = require("../db");
-
+const { authMiddleware } = require("../middleware");
 
 const signupBody = zod.object({
   username: zod.string().email(),
@@ -12,9 +12,14 @@ const signupBody = zod.object({
   password: zod.string(),
 });
 const signinBody = zod.object({
-    username: zod.string().email(),
-    password: zod.string(),
-  });
+  username: zod.string().email(),
+  password: zod.string(),
+});
+const updateBody=zod.object({
+    password:zod.string().optional(),
+    firstName:zod.string().optional(),
+    lastName:zod.string().optional()
+})
 
 const router = Router();
 
@@ -26,7 +31,7 @@ router.post("/signup", async (req, res) => {
     });
   }
   const userExists = await User.findOne({
-    username:req.body.username,
+    username: req.body.username,
   });
   if (userExists) {
     return res.status(411).json({
@@ -39,7 +44,7 @@ router.post("/signup", async (req, res) => {
     lastName: req.body.lastName,
     password: req.body.password,
   });
-  const userId=user._id
+  const userId = user._id;
 
   const token = jwt.sign({ userId }, JWT_SECRET);
   return res.status(200).json({
@@ -48,27 +53,72 @@ router.post("/signup", async (req, res) => {
   });
 });
 
-router.post("/signin",async (req,res)=>{
-    const {success}=signinBody.safeParse(req.body)
-    if (!success) {
-        return res.status(411).json({
-            message: "Error while logging in"
-        });
-      }
-    const user=await User.findOne({
-        username:req.body.username,
-        password:req.body.password
-    })
-    if(user){
-    const userId=user._id
-    const token=jwt.sign({userId},JWT_SECRET);
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Error while logging in",
+    });
+  }
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  if (user) {
+    const userId = user._id;
+    const token = jwt.sign({ userId }, JWT_SECRET);
     return res.status(200).json({
-        token: token,
+      token: token,
+    });
+  }
+  return res.status(411).json({
+    message: "Error while logging in",
+  });
+});
+
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if (!success) {
+      return res.status(411).json({
+        message: "Error while updating information"
       });
     }
-    return res.status(411).json({
-        message: "Error while logging in"
-    });
-} )
+  const userId = req.userId;
+  await User.updateOne(
+    {
+      _id: userId,
+    },
+    req.body
+  );
+  res.status(200).json({
+    message: "Updated successfully"
+
+  })
+});
+
+router.get("/bulk",authMiddleware,async (req,res)=>{
+    const name=req.query.filter|| ""
+
+    const users=await User.find({
+        $or:[{
+            firstName:{
+                "$regex": name
+            },
+            lastName:{
+                "$regex": name
+            },
+        }]
+    })
+    
+    res.status(200).json({
+       user: users.map(user=>({
+            username:user.username,
+            firstName:user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
 
 module.exports = router;
